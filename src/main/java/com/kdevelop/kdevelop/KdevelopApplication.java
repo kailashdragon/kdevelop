ba9d900bc2d7d3fb745f5088cc9b2c68428ee4d7
Kailash <kailashchaudhary9009@gmaail.com>
2017-11-15 00:08:25 +0530

    initialization complete

---
+++ src/main/java/com/kdevelop/kdevelop/KdevelopApplication.java
@@ -0,0 +1,12 @@
+package com.kdevelop.kdevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdevelopApplication.class, args);
+	}
+}
